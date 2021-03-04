beae076ff53d4cca228b026e3890217a1aeedbbe
Yuto Nakamura <yutonakamura0320@gmail.com>
2021-03-04 21:00:17 +0900

    feat(app.tsx): add firebase

--- App.tsx
+++ App.tsx
@@ -1,6 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as firebase from "firebase";
+import "firebase/firestore";
+import Config from "react-native-config";
+
+if (!firebase.app.length) {
+  const firebaseConfig = {
+    apiKey: "Config.API_KEY",
+    authDomain: "Config.AUTH_DOMAIN",
+    projectId: "Config.PROJECT_ID",
+    storageBucket: "Config.STORAGE_BUCKET",
+    messagingSenderId: "Config.MESSAGING_SENDER_ID",
+    appId: "Config.APP_ID",
+    measurementId: "Config.MEASUREMENT_ID",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
+
+console.log(process.env.REACT_APP_API_KEY);
 
 export default function App() {
   return (
@@ -14,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
